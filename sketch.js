831e9190338a96bae7378189a47dcc9011531130
Mark Grimm <mark.grimm.coding@gmail.com>
2022-07-01 13:57:16 +0200

    feat analog clock working

--- sketch.js
+++ sketch.js
@@ -1,7 +1,16 @@
-function setup() {
-  createCanvas(100%, 100%);
-}
+const sec = document.querySelector(".seconds");
+const min = document.querySelector(".minutes");
+const hour = document.querySelector(".hours");
 
-function draw() {
-  background(220);
-}
+setInterval(() => {
+  const date = new Date();
+  console.log(date);
+
+  const secDegree = (date.getSeconds() / 60) * 360;
+  const minDegree = (date.getMinutes() / 60) * 360;
+  const hourDegree = (date.getHours() / 12) * 360;
+
+  hour.style.transform = "rotate(" + String(hourDegree) + "deg)";
+  min.style.transform = "rotate(" + String(minDegree) + "deg)";
+  sec.style.transform = "rotate(" + String(secDegree) + "deg)";
+}, 1000);
